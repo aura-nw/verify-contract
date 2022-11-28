f6dec51b850221452aabf7a1fdaf7b1638eee439
doquockhanhan <doquockhanhan@gmail.com>
2022-11-28 15:13:11 +0700

    update secret name

--- ci/build.sh
+++ ci/build.sh
@@ -2,7 +2,7 @@
 set -xe
 
 #Login to registry
-echo $GITHUB_PASSWORD | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
+echo $REGISTRY_PASSWORD | docker login ghcr.io -u $REGISTRY_USERNAME --password-stdin
 #Build and push image
 docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
 docker push ${CONTAINER_RELEASE_IMAGE}
